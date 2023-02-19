ed5d2cf09e5d55847cab7c75860f0a4ff7b411d4
Sang121 <20010797@st.phenikaa-uni.edu.vn>
2023-02-19 13:31:33 +0700

    Bai tap 1

---
+++ src/TodoApp.jsx
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+
+class TodoApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      tasks: [],
+    };
+    this.handleChangeName = this.handleChangeName.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleEditTask = this.handleEditTask.bind(this);
+    this.handleDeleteTask = this.handleDeleteTask.bind(this);
+  }
+
+  handleChangeName(e) {
+    this.setState({ name: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const task = {
+      name: this.state.name,
+    };
+    this.setState({
+      tasks: [...this.state.tasks, task],
+      name: "",
+    });
+  }
+
+  handleEditTask(e, index) {
+    const tasks = this.state.tasks.slice();
+    tasks[index].name = e.target.value;
+    this.setState({ tasks });
+  }
+
+  handleDeleteTask(index) {
+    const tasks = this.state.tasks.slice();
+    tasks.splice(index, 1);
+    this.setState({ tasks });
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Viết nội dung </h3>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.name} onChange={this.handleChangeName} />
+          <button type="submit">Thêm Task</button>
+        </form>
+        <ul>
+          {this.state.tasks.map((task, index) => (
+            <li key={index}>
+              <input
+                type="text"
+                value={task.name}
+                onChange={(e) => this.handleEditTask(e, index)}
+              />
+              <button onClick={() => this.handleDeleteTask(index)}>Xóa Task</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default TodoApp;
